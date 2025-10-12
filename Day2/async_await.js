4aaa16b86b36b861174dab037d42a175ba8250e4
WaleedAllawie <Waleedallawie@gmail.com>
2025-10-12 16:15:13 +0300

    Refactored the code using async/await in the async_await.js

---
+++ Day2/async_await.js
@@ -0,0 +1,38 @@
+//using promises
+
+const { rejects } = require("assert");
+
+const users = [
+  { name: "user1", id: 1 },
+  { name: "user2", id: 2 },
+  { name: "user3", id: 3 },
+];
+
+function fetchUsers() {
+  setTimeout(() => {
+    users.forEach((user) => {
+      console.log(user);
+    });
+  }, 1000);
+}
+
+function createUser(user) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      users.push(user);
+      const err = false;
+      if (!err) {
+        resolve();
+      } else {
+        reject("There is an error");
+      }
+    }, 2000);
+  });
+}
+
+async function init() {
+  await createUser({ name: "userx", id: "x" });
+  fetchUsers();
+}
+
+init();
