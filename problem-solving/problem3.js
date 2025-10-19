6e1b92714c972ffdd4e716e7cd6afbc0a788602b
WaleedAllawie <Waleedallawie@gmail.com>
2025-10-19 09:44:57 +0300

    Problem 3 solved in problem solving folder

---
+++ problem-solving/problem3.js
@@ -0,0 +1,43 @@
+/*
+ *
+ * Given an array representing prices of the stock on different days,
+ * find the maximum profit that can be earned by performing maximum of
+ * one transaction. A transaction consists of activity of buying and
+ * selling the stock on different or same days.
+ *
+ */
+
+/*
+ *
+ * For example in this array - {100, 80, 120, 130, 70, 60, 100, 125}
+ * the price of the stock on day-1 is 100, on day-2 is 80 and so on.
+ * The maximum profit that could be earned in this window is 65
+ * (buy at 60 and sell at 125).
+ * For price array - {100, 80, 70, 65, 60, 55, 50}, maximum profit
+ * that could be earned is 0.
+ *
+ */
+
+function maximumProfit(array) {
+  let l = 0;
+  let r = 1;
+  let maxProfit = 0;
+
+  while (r < array.length) {
+    let profit = array[r] - array[l];
+
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+
+    if (array[r] < array[l]) {
+      l = r;
+    }
+
+    r++;
+  }
+
+  return maxProfit;
+}
+
+console.log(maximumProfit([100, 80, 120, 130, 70, 60, 100, 125])); // Should return 65
